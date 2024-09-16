0be03dc6d85f585f825e372d32ae722bdfdc5381
suhail-1322 <137207949+suhail-1322@users.noreply.github.com>
2024-09-16 16:12:27 +0530

    Time: 7 ms (52.96%) | Memory: 45.3 MB (63.14%) - LeetSync

--- 539-minimum-time-difference/minimum-time-difference.java
+++ 539-minimum-time-difference/minimum-time-difference.java
@@ -9,15 +9,19 @@ class Solution {
         }
         Arrays.sort(arr);
         int min = Integer.MAX_VALUE;
-        for(int i = 0;i<n;i++){
-            for(int j = i+1;j<n;j++){
-                int dif = Math.min(arr[i]+1440-arr[j],arr[j]-arr[i]);
-                if(dif < min){
-                    min = dif;
-                }
-            }
+        // for(int i = 0;i<n;i++){
+        //     for(int j = i+1;j<n;j++){
+        //         int dif = Math.min(arr[i]+1440-arr[j],arr[j]-arr[i]);
+        //         if(dif < min){
+        //             min = dif;
+        //         }
+        //     }
             
+        // }
+        for(int i = 1;i<n;i++){
+            int res = arr[i]-arr[i-1];
+            if(res < min) min = res;
         }
-        return min;
+        return Math.min(min,arr[0]+1440-arr[n-1]);
     }
 }
