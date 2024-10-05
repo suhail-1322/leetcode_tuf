a996de0e6aff8b6eeaf085a9d8544c21b5ae4acd
suhail-1322 <137207949+suhail-1322@users.noreply.github.com>
2024-10-05 21:50:59 +0530

    Time: 16 ms (23.83%) | Memory: 45 MB (8.51%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+
+        HashMap<Character,Integer> map = new HashMap<>();
+
+        for(char c : s.toCharArray()){
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
+
+        for(char c : t.toCharArray()){
+            if(map.containsKey(c) && map.get(c) > 0){
+                map.put(c,map.get(c)-1);
+            }
+            else return false;
+        }
+
+        // for(Integer val : map.values()){
+        //     if(val != 0) return false;
+        // }
+        return true;
+        
+    }
+}
