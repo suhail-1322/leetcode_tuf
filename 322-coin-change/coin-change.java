3bfe6fb9ae5ea3b01d942ceee36307ad74a45751
suhail-1322 <137207949+suhail-1322@users.noreply.github.com>
2024-11-13 09:09:42 +0530

    Time: 16 ms (56.76%) | Memory: 45 MB (27.09%) - LeetSync

---
+++ 322-coin-change/coin-change.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int n = coins.length;
+        int[][] dp = new int[n + 1][amount + 1];
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 0;
+        }
+
+        // Initialize dp[0][j] to a large number for all j > 0, as no amount > 0 can be made with zero coins
+        for (int j = 1; j <= amount; j++) {
+            dp[0][j] = Integer.MAX_VALUE - 1;
+        }
+
+
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < amount + 1; j++) {
+                if (coins[i - 1] <= j) {
+                    dp[i][j] = Math.min(1 + dp[i][j - coins[i - 1]], dp[i - 1][j]);
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[n][amount] == Integer.MAX_VALUE - 1 ? -1 : dp[n][amount];
+    }
+}
